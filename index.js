a9c4df0458837a7e6c3f3bea6e2505eabe27b7f8
Ana Zitzow <anasempai89@gmail.com>
2022-04-27 17:22:42 -0700

    commit tree 495dc4bb9ca59b2e58daf8a4047bb95ef8581bf8

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const writeCards = (names, message = 'surprise') => {
+  const messageArray = [];
+  for (let i = 0; i < names.length; i++) {
+    messageArray.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`);
+  }
+  return messageArray;
+}
+
+// const countDown = (num) => {
+//   for (let i = num; i >= 0; i--){
+//     console.log(i);
+//   }
+// }
+const countDown = (num) => {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
